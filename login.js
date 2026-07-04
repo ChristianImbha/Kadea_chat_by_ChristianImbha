c78fec10fa517661015181bb83d8a0f9af87389a
Christian Imbha <christianimbha@gmail.com>
2026-07-04 18:39:26 +0100

    feat(auth): intercepter la soumission du formulaire de connexion

---
+++ login.js
@@ -0,0 +1,5 @@
+// On attend que le DOM soit complètement chargé
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const togglePasswordBtn = document.querySelector('.btn-toggle-password');
+    const passwordInput = document.getElementById('password');
