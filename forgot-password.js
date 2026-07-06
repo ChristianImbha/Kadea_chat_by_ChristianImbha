f10a6f5a54d7aa22eeb06a7b9f117ffd591475ee
Christian Imbha <christianimbha@gmail.com>
2026-07-06 18:53:42 +0100

    feat(auth): initialiser la logique de oubli de mot de passe et lier le DOM

---
+++ forgot-password.js
@@ -0,0 +1,6 @@
+// Initialisation des configurations et liaison au DOM
+const API_KEY = 'TON_API_KEY'; 
+const BASE_URL = 'URL_DE_L_API_KADEA';
+
+const forgotForm = document.getElementById('forgot-form');
+const resetForm = document.getElementById('reset-form');
